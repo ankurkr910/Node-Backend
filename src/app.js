33aed53f68ce7856e7713f0c9bd504904e7fd76c
Ankur <ankurkr910@gmail.com>
2025-09-11 14:33:39 +0530

    initial db setup

--- src/app.js
+++ src/app.js
@@ -1,13 +1,36 @@
 const express = require('express');
+const connectDB = require('./config/database');
+const User = require('./models/user');
+const app = express();
+const PORT = 7777;
+
+app.use(express.json());
+
+
+app.post('/api/signUp', async (req, res) => {
+    const user = new User({
+        firstName: 'Ankur',
+        lastName: 'Kumar',
+        emailId: 'abc@gmail.com'
+    })
+    try{
+        await user.save();
+        res.status(200).json({message: 'User registered successfully'})
+    } catch{
+        res.status(500).json({message: 'Error registering user', error: err})
+    }
  
 
-const app = express();
+})
 
-const PORT = 7777;
-app.use((req, res, next) => {
-    res.send('Hello from Node.js server!'); // Simple response for all requi
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+ 
+connectDB().then(() => {
+    console.log('Database connection established');
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  }
+  ).catch((err) => {
+    console.error('Database connection error:', err);
+  }
+  );
